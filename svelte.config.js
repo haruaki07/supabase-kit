8ce7f8fb0ba1c8109fd20d50d0bf585f0433e82e
haruaki07 <misbahulk2236@gmail.com>
2021-08-01 19:41:26 +0700

    fix gh-pages, thanks to https://svelteland.github.io/svelte-kit-blog-demo/deply-to-github/

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,18 @@
 import adapter from "@sveltejs/adapter-static";
 
+const path = "/supabase-kit"; // for gh-pages
+const prod = process.env.NODE_ENV === 'production';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+    // fix gh-pages assets not loaded
+    ...(prod && {
+      paths: {
+        assets: path,
+        base: path
+      }
+    }),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
     adapter: adapter({
